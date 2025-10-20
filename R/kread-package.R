ac7b4c8e52cb8505489f1ce8816a18d00908764f
Kevin Bosirany ORLANDO <kevinbosirany@gmail.com>
2025-10-20 16:37:26 +0200

    refactor: remove obsolete utility functions
    - Remove cleanDataCache(), getDataPath(), and loadConfig()
    - Remove kread-package.R
    - These functions were not related to the core file reading purpose
    - Simplifies package focus on file reading functionality

--- R/kread-package.R
+++
@@ -1,9 +0,0 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-## usethis namespace: end
-
-utils::globalVariables(".")
-
-NULL
--- R/loadConfig.R
+++
@@ -1,7 +0,0 @@
-#' @inherit fairify::loadConfig
-#' @export
-loadConfig <- function(userFile = "config.yml",
-                       pathDefaultCfg = system.file("config.yml", package = utils::packageName()),
-                       ...) {
-  fairify::loadConfig(userFile = userFile, pathDefaultCfg = pathDefaultCfg, ...)
-}
